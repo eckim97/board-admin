60b72a90fa209c98a4d25cb4c6bce3d990812762
kec <weoncheol1@naver.com>
2024-08-24 00:08:37 +0900

    #24 - 회원 관리 기능 컨트롤러, 서비스, 뷰 구현
    컨트롤러에서 회원 관리 페이지 요청을 처리하고, 유저 목록을 조회하여 뷰에 전달하는 기능을 구현.
    
    서비스에서는 RestTemplate을 사용해 외부 API에서 회원 정보를 가져와 비즈니스 로직을 처리. 특정 유저 조회 및 삭제 기능도 포함.
    
    API 응답 데이터를 효율적으로 다루기 위해 UserAccountResponse DTO를 추가하여 필요한 데이터만 선택적으로 제공.
    
    뷰에서는 타임리프를 활용하여 유저 목록을 테이블 형태로 출력하고, 유저 ID를 클릭하면 모달 창에서 세부 정보를 확인할 수 있도록 구성.
    
    또한, 타임리프의 디커플드 로직을 사용해 공통 레이아웃과 모달 구조를 외부 파일로 분리하여 재사용성과 유지보수성을 높임.

--- src/main/java/org/example/projectboardadmin/controller/UserAccountManagementController.java
+++ src/main/java/org/example/projectboardadmin/controller/UserAccountManagementController.java
@@ -1,23 +1,38 @@
 package org.example.projectboardadmin.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
+import lombok.RequiredArgsConstructor;
+import org.example.projectboardadmin.dto.response.UserAccountResponse;
+import org.example.projectboardadmin.service.UserAccountManagementService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RequestMapping("/management/user-accounts")
 @Controller
 public class UserAccountManagementController {
 
+    private final UserAccountManagementService userAccountManagementService;
+
     @GetMapping
-    public String userAccounts(
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
-    ) {
+    public String userAccounts(Model model) {
+        model.addAttribute(
+                "userAccounts",
+                userAccountManagementService.getUserAccounts().stream().map(UserAccountResponse::from).toList()
+        );
         return "management/user-accounts";
     }
 
+    @ResponseBody
+    @GetMapping("/{userId}")
+    public UserAccountResponse userAccount(@PathVariable String userId) {
+        return UserAccountResponse.from(userAccountManagementService.getUserAccount(userId));
+    }
+
+    @PostMapping("/{userId}")
+    public String deleteUserAccount(@PathVariable String userId) {
+        userAccountManagementService.deleteUserAccount(userId);
+
+        return "redirect:/management/user-accounts";
+    }
 }
--- src/main/java/org/example/projectboardadmin/service/UserAccountManagementService.java
+++ src/main/java/org/example/projectboardadmin/service/UserAccountManagementService.java
@@ -2,23 +2,49 @@ package org.example.projectboardadmin.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.projectboardadmin.dto.UserAccountDto;
+import org.example.projectboardadmin.dto.properties.ProjectProperties;
+import org.example.projectboardadmin.dto.response.UserAccountClientResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import java.util.NoSuchElementException;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserAccountManagementService {
 
-    public List<UserAccountDto> getUserAccounts() {
-        return List.of();
+    private final RestTemplate restTemplate;
+
+    private final ProjectProperties projectProperties;
+
+    public List<UserAccountDto> getUserAccounts(){
+        URI uri = UriComponentsBuilder.fromHttpUrl(projectProperties.board().url() + "/api/userAccounts")
+                .queryParam("size", 10000) // TODO: 전체 회원 목록을 가져오기 위해 충분히 큰 사이즈를 전달하는 방식. 불완전하다.
+                .build()
+                .toUri();
+        UserAccountClientResponse response = restTemplate.getForObject(uri, UserAccountClientResponse.class);
+
+        return Optional.ofNullable(response).orElseGet(UserAccountClientResponse::empty).userAccounts();
     }
 
     public UserAccountDto getUserAccount(String userId) {
-        return null;
+        URI uri = UriComponentsBuilder.fromHttpUrl(projectProperties.board().url() + "/api/userAccounts/" + userId)
+                .build()
+                .toUri();
+        UserAccountDto response = restTemplate.getForObject(uri, UserAccountDto.class);
+
+        return Optional.ofNullable(response)
+                .orElseThrow(() -> new NoSuchElementException("게시글이 없습니다 - userId: " + userId));
     }
 
     public void deleteUserAccount(String userId) {
-
+        URI uri = UriComponentsBuilder.fromHttpUrl(projectProperties.board().url() + "/api/userAccounts/" + userId)
+                .build()
+                .toUri();
+        restTemplate.delete(uri);
     }
 }
